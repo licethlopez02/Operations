217be782b35cc939ddeea0a1f4cb1bfa69754680
licethlopez02 <liceth.lopez02@uptc.edu.co>
2026-08-11 21:10:37 -0500

    Initialize a Spring Boot project

---
+++ src/test/java/co/edu/uptc/Operations/OperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.Operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
